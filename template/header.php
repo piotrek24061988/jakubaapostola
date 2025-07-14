75594efe9c1461b59a424d4b6e33b9f4a6e9ef1f
piotrek24061988 <piotrek24061988@gmail.com>
2025-07-14 13:15:21 +0200

    cleanup tabs -> spaces, icon added etc

--- template/header.php
+++ template/header.php
@@ -7,14 +7,14 @@ echo<<<END
     <meta name="keywords" content="Jakuba Apostoła, Jakuba Apostoła Wielowicz, Parafia Jakuba Apostoła, Kościół Jakuba Apostoła, Biuro Parafialne Jakuba Apostoła, Oficjalna strona Parafii Św. Jakuba Apostoła w Wielowiczu">
     <meta name="author" content="Piotr Górecki">
 
-    <link rel="shortcut icon" href="media/sja.jpg">
-	
+    <link rel="shortcut icon" href="media/jakub/swj_ikona.jpg">
+
     <!-- CSS -->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="static/glowny.css">
 
     <title>Parafia Świętego Jakuba Apostoła w Wielowiczu</title>
-	<link href="https://piotraipawla.bydgoszcz.pl" rel="alternate" hreflang="pl-PL">
+    <link href="https://piotraipawla.bydgoszcz.pl" rel="alternate" hreflang="pl-PL">
     <link href="https://piotrek24061988.pythonanywhere.com" rel="alternate" hreflang="pl-PL">
 
 END;

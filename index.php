eb6dcc3d67f78ebd9f7590922f5a3bbb40331033
piotrek24061988 <piotrek24061988@gmail.com>
2025-07-14 12:26:26 +0200

    initial commit website

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+
+	<head>
+		<?php include 'template/header.php'; ?>
+	</head>
+
+    <body>
+	
+        <main class="container" style="min-height: 100vh;">
+		<h1>Parafia Rzymskokatolicka świętego Jakuba Apostoła w Wielowiczu</h1>
+
+			<?php
+				@session_start();
+			
+				require_once "template/connect.php";
+				mysqli_report(MYSQLI_REPORT_STRICT);
+				try
+				{
+					$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+					if($polaczenie->connect_errno == 0)
+					{
+						$sql = "SELECT * FROM aktualnosci ORDER BY id DESC LIMIT 5";
+						$rezultat = @$polaczenie->query($sql);
+						if(!$rezultat) throw new Exception($polaczenie->error);
+							$counter = 1;
+							while($wiersz = $rezultat->fetch_assoc())
+							{
+								echo '<div class="text-center">';
+								echo '<h2>'.$counter.'</h2>';
+								
+								echo '<div class="my-img mb-5">';
+								echo '<h2>'.$wiersz['tytul'].'<h2>';
+								echo '<h2>'.$wiersz['zdjecie1'].'</h2>';
+								echo '<img src="'.$wiersz['zdjecie1'].'" alt="'.$wiersz['tytul'].'" class="img-fluid"/>';
+								echo '</div>';
+								
+								echo '<div class="my-img mb-5">';
+								echo '<h2>'.$wiersz['tresc'].'</h2>';
+								echo '<h2>'.$wiersz['zdjecie2'].'</h2>';
+								echo '<img src="'.$wiersz['zdjecie2'].'" alt="'.$wiersz['tresc'].'" class="img-fluid"/>';
+								echo '</div>';
+								
+								echo '</div>';
+								
+								$counter++;
+							}					  						
+				
+						$polaczenie->close();
+					}
+				}
+				catch(Exception $e)
+				{
+					echo '<div class="text-danger"><b>Błąd serwera. Nie można nawiązać połączenia z bazą danych</b></div>';
+					exit();
+				}			
+			?>
+
+        </main>
+		
+		<?php include 'template/footer.php'; ?>
+    </body>
+
+</html>
---
+++ template/header.php
@@ -0,0 +1,21 @@
+<?php
+echo<<<END
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Oficjalna strona Parafii Św. Jakuba Apostoła w Wielowiczu">
+    <meta name="keywords" content="Jakuba Apostoła, Jakuba Apostoła Wielowicz, Parafia Jakuba Apostoła, Kościół Jakuba Apostoła, Biuro Parafialne Jakuba Apostoła, Oficjalna strona Parafii Św. Jakuba Apostoła w Wielowiczu">
+    <meta name="author" content="Piotr Górecki">
+
+    <link rel="shortcut icon" href="media/sja.jpg">
+	
+    <!-- CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="static/glowny.css">
+
+    <title>Parafia Świętego Jakuba Apostoła w Wielowiczu</title>
+	<link href="https://piotraipawla.bydgoszcz.pl" rel="alternate" hreflang="pl-PL">
+    <link href="https://piotrek24061988.pythonanywhere.com" rel="alternate" hreflang="pl-PL">
+
+END;
+?>
